656526fe95386b22c7b29b0ee21b06d24cd53f2b
kurama <52566613+KuramaSyu@users.noreply.github.com>
2025-11-28 16:31:56 +0100

    api!: now optional types

--- src/db/entities/note/metadata.py
+++ src/db/entities/note/metadata.py
@@ -1,5 +1,5 @@
 from dataclasses import dataclass
-from typing import List
+from typing import List, Optional
 from datetime import datetime
 
 from .embedding import NoteEmbeddingEntity
@@ -8,10 +8,10 @@ from .permission import NotePermissionEntity
 @dataclass
 class NoteEntity:
     """Represents one record of note.metadata"""
-    note_id: int
-    title: str
-    updated_at: datetime
-    author_id: int
-    content: str
+    note_id: Optional[int]
+    title: Optional[str]
+    updated_at: Optional[datetime]
+    author_id: Optional[int]
+    content: Optional[str]
     embeddings: List[NoteEmbeddingEntity]
     permissions: List[NotePermissionEntity]
