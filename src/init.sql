00021b86defa42935a778d092b3e8b066a85686a
kurama <52566613+KuramaSyu@users.noreply.github.com>
2025-11-28 19:27:40 +0100

    fix: wrong order and not working names

--- src/init.sql
+++ src/init.sql
@@ -1,40 +1,41 @@
 CREATE EXTENSION IF NOT EXISTS vector;
 
-CREATE TABLE IF NOT EXISTS user (
+CREATE TABLE IF NOT EXISTS users (
     id SERIAL PRIMARY KEY,
     discord_id BIGINT,
     avatar_url TEXT
 );
 
 CREATE SCHEMA IF NOT EXISTS note;
+CREATE SCHEMA IF NOT EXISTS "role";
+CREATE TABLE IF NOT EXISTS role.metadata (
+    id SERIAL PRIMARY KEY,
+    name TEXT,
+    description TEXT
+);
+
+
 CREATE TABLE IF NOT EXISTS note.content (
     id SERIAL PRIMARY KEY,
     title TEXT,
     content TEXT,
     updated_at TIMESTAMP,
-    author_id BIGINT
+    author_id BIGINT REFERENCES users(id)
 );
 
 CREATE TABLE IF NOT EXISTS note.embedding (
-    note_id BIGINT REFERENCES note.metadata(id),
+    note_id BIGINT REFERENCES note.content(id),
     model VARCHAR(128),
     embedding VECTOR(384), -- size of output of text-embedding-3-small model 
     PRIMARY KEY(note_id, model)
 );
 
 CREATE TABLE IF NOT EXISTS note.permission (
-    note_id BIGINT REFERENCES note.metadata(id),
+    note_id BIGINT REFERENCES note.content(id),
     role_id BIGINT REFERENCES role.metadata(id),
     PRIMARY KEY(note_id, role_id)
 );
 
-
-CREATE SCHEMA IF NOT EXISTS role;
-CREATE TABLE IF NOT EXISTS role.metadata (
-    id SERIAL PRIMARY KEY,
-    name TEXT,
-    description TEXT
-);
 
 CREATE TABLE IF NOT EXISTS role.permission (
     id SERIAL PRIMARY KEY,
@@ -45,6 +46,6 @@ CREATE TABLE IF NOT EXISTS role.permission (
 
 CREATE TABLE IF NOT EXISTS role.member (
     role_id BIGINT REFERENCES role.metadata(id),
-    member_id BIGINT REFERENCES user(id),
+    member_id BIGINT REFERENCES users(id),
     PRIMARY KEY(role_id, member_id)
 );
