31355852a2642bf9c6825b21fc792dcc341f50b4
kurama <52566613+KuramaSyu@users.noreply.github.com>
2025-12-08 23:06:23 +0100

    chore: updated imports

--- src/__init__.py
+++ src/__init__.py
@@ -1,2 +1,5 @@
+from .ai import *
 from .api import loggingProvider
 from .db import repos, entities
+from .grpc_mod import converter
+from .grpc_mod import service
--- src/grpc_mod/converter/__init__.py
+++ src/grpc_mod/converter/__init__.py
@@ -1,2 +1,3 @@
+from .dict_helper import drop_undefined
 from .note_entity_converter import to_grpc_note
 from .user_entity_converter import to_grpc_user
