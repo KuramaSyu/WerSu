52b9b658cd3150e396030276835812f140cfda8b
kurama <paul.zenker.pz@gmail.com>
2025-12-11 14:59:28 +0100

    fix: only drop undefined values

--- src/utils/dict_helper.py
+++ src/utils/dict_helper.py
@@ -3,13 +3,13 @@ from api.undefined import UNDEFINED
 
 
 def drop_undefined(data: Dict[str, Any]) -> Dict[str, Any]:
-    """Recursively drops fields with value UNDEFINED and as false interpreted values from a dictionary."""
+    """Recursively drops fields with value UNDEFINED"""
     if not isinstance(data, dict):
         return data
     return {
         key: drop_undefined(value)
         for key, value in data.items()
-        if value is not UNDEFINED and value not in (None, '', [], {}, ())
+        if value is not UNDEFINED
     }
 
 def drop_except_keys(data: Dict[str, Any], keys_to_keep: set) -> Dict[str, Any]:
