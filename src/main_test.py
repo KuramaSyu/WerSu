27cd98af9980fdb00816d0cab6cacdc49f74ff17
kurama <paul.zenker.pz@gmail.com>
2025-12-13 19:02:04 +0100

    docs: added comment

--- src/main_test.py
+++ src/main_test.py
@@ -1,4 +1,10 @@
 import unittest
-from tests.dict_helper import DropUndefinedUseCase, DropExceptKeysUseCase, AsDictDataclassUseCase
+
+# import all test cases, otherwise unittest won't find them
+from tests.dict_helper import (
+    DropUndefinedUseCase, 
+    DropExceptKeysUseCase, 
+    AsDictDataclassUseCase
+)
 if __name__ == "__main__":
     unittest.main()
