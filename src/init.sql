ef977b8aec843662bf94f15e29851aa5e029a7f6
kurama <paul.zenker.pz@gmail.com>
2025-12-18 19:16:08 +0100

    api!: improved role schema

--- src/init.sql
+++ src/init.sql
@@ -30,11 +30,13 @@ CREATE TABLE IF NOT EXISTS note.embedding (
     PRIMARY KEY(note_id, model)
 );
 
+-- available permissions
 CREATE TABLE IF NOT EXISTS role.permission (
     id BIGINT PRIMARY KEY,
     key TEXT UNIQUE -- e.g., 'read', 'write', 'delete', 'manage_roles'
 );
 
+-- default permissions for a role / for now not important
 CREATE TABLE IF NOT EXISTS role.role_permission (
     role_id BIGINT REFERENCES role.role(id),
     permission_id BIGINT REFERENCES role.permission(id),
@@ -42,23 +44,34 @@ CREATE TABLE IF NOT EXISTS role.role_permission (
     PRIMARY KEY(role_id, permission_id)
 );
 
+-- roles
 CREATE TABLE IF NOT EXISTS role.role (
   id BIGINT PRIMARY KEY,
   name TEXT,
   description TEXT
 );
 
+-- resources (shelves, books, chapters, notes) and their hierarchy
 CREATE TABLE IF NOT EXISTS role.resource (
     id BIGSERIAL PRIMARY KEY,
     type TEXT NOT NULL CHECK (type IN ('shelf', 'book', 'chapter', 'note')),
-    parent_id BIGINT REFERENCES role.resource(id),
+    parent_id BIGINT REFERENCES role.resource(id) NULL,
     name TEXT NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS role.resource_role_assignment (
+-- assigns permissiont to a role for a specific resource
+CREATE TABLE IF NOT EXISTS role.resource_role_permission_assignment (
+    id BIGINT PRIMARY KEY,
+    permission_id BIGINT REFERENCES role.permission(id),
+    role_id BIGINT REFERENCES role.role(id),
+    resource_id BIGINT NOT NULL,
+    UNIQUE(role_id, resource_id, permission_id)
+);
+
+-- assigns a role to a user
+CREATE TABLE IF NOT EXISTS role.user_role_assignment (
     id BIGINT PRIMARY KEY,
     user_id BIGINT REFERENCES users(id),
     role_id BIGINT REFERENCES role.role(id),
-    resource_id BIGINT NOT NULL,
-    UNIQUE(user_id, role_id, resource_id)
-)
+    UNIQUE(user_id, role_id)
+);
