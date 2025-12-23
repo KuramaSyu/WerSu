872d1ca1297cb1d881f9e0d2c64a21ecba4ac6f3
kurama <paul.zenker.pz@gmail.com>
2025-12-23 22:34:46 +0100

    tests: improved user_repo test

--- tests/test_user_repo.py
+++ tests/test_user_repo.py
@@ -1,63 +1,42 @@
 from dataclasses import replace
 from typing import AsyncGenerator, Optional
 import pytest
+from sqlalchemy import update
 from testcontainers.postgres import PostgresContainer
 from src.db.entities.user.user import UserEntity
 from src.db.repos.user.user import UserRepoABC
 import src.api
 from src.db.repos import UserPostgresRepo, Database
 from src.utils import logging_provider
-
-def create_postgres_dsn(postgres_container: PostgresContainer) -> str:
-    return (
-        f"postgresql://"
-        f"{postgres_container.username}:"
-        f"{postgres_container.password}@"
-        f"{postgres_container.get_container_host_ip()}:"
-        f"{postgres_container.get_exposed_port(5432)}/"
-        f"{postgres_container.dbname}"
-    )
-
-@pytest.fixture
-async def db():
-    container = PostgresContainer(
-        image="pgvector/pgvector:pg16",
-        username="postgres",
-        password="postgres",
-        dbname="testdb",
-    )
-    container.start()
-    dsn = create_postgres_dsn(container)
-    db = Database(dsn, logging_provider, init_file="src/init.sql")
-    await db.init_db()
-    yield db
-    await db.close()
-    container.stop()
+from .fixtures import db, user_repo
 
 
-async def test_create_user(db: Database):
-    repo: UserRepoABC = UserPostgresRepo(db)
+
+async def test_create_user(db: Database, user_repo: UserRepoABC):
+    """Creates a test user and retrieves it by discord_id"""
     test_user = UserEntity(
         discord_id=123455,
         avatar_url="test",
     )
-    await repo.insert(test_user)
-    ret_user = await repo.select_by_discord_id(test_user.discord_id)
+    await user_repo.insert(test_user)
+    ret_user = await user_repo.select_by_discord_id(test_user.discord_id)
     assert ret_user
     assert ret_user.avatar_url == test_user.avatar_url
 
-async def test_update_user(db: Database):
-    repo: UserRepoABC = UserPostgresRepo(db)
+async def test_update_user(db: Database, user_repo: UserRepoABC):
+    """Creates a test user, updates it, and retrieves it once by discord_id and once by id"""
     test_user = UserEntity(
         discord_id=123455,
         avatar_url="test",
     )
-    await repo.insert(test_user)
+    await user_repo.insert(test_user)
     updated_user = replace(test_user, avatar_url="http://somewere")
-    await repo.update(updated_user)
-    ret_user = await repo.select_by_discord_id(updated_user.discord_id)
-    assert ret_user
-    assert ret_user == updated_user  # now also id should match
-
+    ret_user_update = await user_repo.update(updated_user)
+    ret_user_discord = await user_repo.select_by_discord_id(updated_user.discord_id)
+    assert ret_user_discord and ret_user_discord.id
+    assert ret_user_discord == ret_user_update  # assert that update returns same as select
+    assert ret_user_discord == updated_user  # now also id should match
+    ret_user_by_id = await user_repo.select(ret_user_discord.id)
+    assert ret_user_by_id == ret_user_discord  # both selects should return same user
 
 
