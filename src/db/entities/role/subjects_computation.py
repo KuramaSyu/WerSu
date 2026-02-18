163ea0518ee656d09be28bc4e7a2b86b9b10e804
kurama <52566613+KuramaSyu@users.noreply.github.com>
2026-02-18 16:25:53 +0100

    feat: added concrete Expressions

--- src/db/entities/role/subjects_computation.py
+++ src/db/entities/role/subjects_computation.py
@@ -1,30 +1,6 @@
 from abc import ABC, abstractmethod
 from enum import Enum
-class ValidationError(Exception):
-    ...
-
-class SubjectsComputationExpression(ABC):
-    """Abstract Base Class defining how Subjects are computed"""
-    @abstractmethod
-    def is_subjects_computation_expression():
-        ...
-
-    @abstractmethod
-    def operation():
-        ...
-
-    @abstractmethod
-    def validate():
-        """
-        Raises
-        -------
-        ValidationError: if validation fails
-        """
-        ...
-
-    @abstractmethod
-    def __str__(self) -> str:
-        ...
+from typing import Type
 
 class SubjectsComputationOp(Enum):
     """Possible operations a subject can use"""
@@ -33,3 +9,64 @@ class SubjectsComputationOp(Enum):
     OP_EXCLUSION = "EXCLUSION"
     OP_COMPUTED_SUBJECTS = "COMPUTED_SUBJECTS"  # for same-object inheritance
     OP_DIRECT = "DIRECT"  # No operation but direct assignment
+
+
+class ValidationError(Exception):
+    ...
+
+
+class SubjectsComputationExpressionABC(ABC):
+    """Abstract Base Class defining how Subjects are computed"""
+    # @abstractmethod
+    # def is_subjects_computation_expression():
+    #     ...
+
+    @abstractmethod
+    def operation(self) -> SubjectsComputationOp:
+        ...
+
+    # @abstractmethod
+    # def validate():
+    #     """
+    #     Raises
+    #     -------
+    #     ValidationError: if validation fails
+    #     """
+    #     ...
+
+    # @abstractmethod
+    # def __str__(self) -> str:
+    #     ...
+
+
+
+class DirectExpression(SubjectsComputationExpressionABC):
+    def operation(self) -> SubjectsComputationOp:
+        return SubjectsComputationOp.OP_DIRECT
+
+
+class ComputedSubjectsExpression(SubjectsComputationExpressionABC):
+    """Computed relations: include subjects from another relation (same-object inheritance)"""
+    computed_relation: str  # "owner" (on the same object)
+    
+    def operation(self) -> SubjectsComputationOp:
+        return SubjectsComputationOp.OP_COMPUTED_SUBJECTS
+    
+
+class UnionExpression(SubjectsComputationExpressionABC):
+    """Boolean OR logic (either set A or set B)"""
+    def operation(self) -> SubjectsComputationOp:
+        return SubjectsComputationOp.OP_UNION
+
+
+class IntersectionExpression(SubjectsComputationExpressionABC):
+    """Boolean AND logic (in both sets A and B)"""
+    def operation(self) -> SubjectsComputationOp:
+        return SubjectsComputationOp.OP_INTERSECTION
+
+
+class ExclusionExpression(SubjectsComputationExpressionABC):
+    """Set substraction A - B (same as A\B)"""
+    def operation(self) -> SubjectsComputationOp:
+        return SubjectsComputationOp.OP_EXCLUSION
+
