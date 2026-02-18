aa12bd14b9da35285cc3bde71005b621b8b66256
kurama <52566613+KuramaSyu@users.noreply.github.com>
2026-02-18 16:13:23 +0100

    chore: rename

--- src/db/entities/role/subjects_computation.py
+++ src/db/entities/role/subjects_computation.py
@@ -1,9 +1,10 @@
 from abc import ABC, abstractmethod
-
+from enum import Enum
 class ValidationError(Exception):
     ...
 
 class SubjectsComputationExpression(ABC):
+    """Abstract Base Class defining how Subjects are computed"""
     @abstractmethod
     def is_subjects_computation_expression():
         ...
@@ -24,3 +25,11 @@ class SubjectsComputationExpression(ABC):
     @abstractmethod
     def __str__(self) -> str:
         ...
+
+class SubjectsComputationOp(Enum):
+    """Possible operations a subject can use"""
+    OP_UNION = "UNION"
+    OP_INTERSECTION = "INTERSECTION"
+    OP_EXCLUSION = "EXCLUSION"
+    OP_COMPUTED_SUBJECTS = "COMPUTED_SUBJECTS"  # for same-object inheritance
+    OP_DIRECT = "DIRECT"  # No operation but direct assignment
