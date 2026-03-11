cd2003d226dd3d62ed1078ccf71ccfd3dd51bcc2
kurama <52566613+KuramaSyu@users.noreply.github.com>
2026-03-11 18:23:58 +0100

    chore: fully removed roles from schema because of spiceDB
    #7

--- src/init.sql
+++ src/init.sql
@@ -11,13 +11,6 @@ CREATE TABLE IF NOT EXISTS users (
 );
 
 CREATE SCHEMA IF NOT EXISTS note;
-CREATE SCHEMA IF NOT EXISTS "role";
-CREATE TABLE IF NOT EXISTS role.metadata (
-    id SERIAL PRIMARY KEY,
-    name TEXT,
-    description TEXT
-);
-
 
 CREATE TABLE IF NOT EXISTS note.content (
     id SERIAL PRIMARY KEY,
@@ -54,49 +47,3 @@ CREATE TABLE IF NOT EXISTS note.embedding (
     PRIMARY KEY(note_id, model)
 );
 
--- available permissions
-CREATE TABLE IF NOT EXISTS role.permission (
-    id BIGINT PRIMARY KEY,
-    key TEXT UNIQUE -- e.g., 'read', 'write', 'delete', 'manage_roles'
-);
-
-
--- roles
-CREATE TABLE IF NOT EXISTS role.role (
-  id BIGINT PRIMARY KEY,
-  name TEXT,
-  description TEXT
-);
-
--- default permissions for a role / for now not important
-CREATE TABLE IF NOT EXISTS role.role_permission (
-    role_id BIGINT NOT NULL REFERENCES role.role(id) ON DELETE CASCADE ON UPDATE CASCADE,
-    permission_id BIGINT NOT NULL REFERENCES role.permission(id) ON DELETE CASCADE ON UPDATE CASCADE,
-    allow BOOLEAN, -- allow/deny flag
-    PRIMARY KEY(role_id, permission_id)
-);
-
--- resources (shelves, books, chapters, notes) and their hierarchy
-CREATE TABLE IF NOT EXISTS role.resource (
-    id BIGSERIAL PRIMARY KEY,
-    type TEXT NOT NULL CHECK (type IN ('shelf', 'book', 'chapter', 'note')),
-    parent_id BIGINT REFERENCES role.resource(id) ON DELETE CASCADE ON UPDATE CASCADE,
-    name TEXT NOT NULL
-);
-
--- assigns permissiont to a role for a specific resource
-CREATE TABLE IF NOT EXISTS role.resource_role_permission_assignment (
-    id BIGINT PRIMARY KEY,
-    permission_id BIGINT NOT NULL REFERENCES role.permission(id) ON DELETE CASCADE ON UPDATE CASCADE,
-    role_id BIGINT NOT NULL REFERENCES role.role(id) ON DELETE CASCADE ON UPDATE CASCADE,
-    resource_id BIGINT NOT NULL REFERENCES role.resource(id) ON DELETE CASCADE ON UPDATE CASCADE,
-    UNIQUE(role_id, resource_id, permission_id)
-);
-
--- assigns a role to a user
-CREATE TABLE IF NOT EXISTS role.user_role_assignment (
-    id BIGINT PRIMARY KEY,
-    user_id BIGINT NOT NULL REFERENCES users(id) ON DELETE CASCADE ON UPDATE CASCADE,
-    role_id BIGINT NOT NULL REFERENCES role.role(id) ON DELETE CASCADE ON UPDATE CASCADE,
-    UNIQUE(user_id, role_id)
-);
