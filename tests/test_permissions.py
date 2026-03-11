e46023da9bdcfe819b52dde39eadbed79a831b7b
kurama <52566613+KuramaSyu@users.noreply.github.com>
2026-03-11 18:00:17 +0100

    chore: switch from sync to async

--- tests/test_permissions.py
+++ tests/test_permissions.py
@@ -12,7 +12,7 @@ from authzed.api.v1 import (
     BulkImportRelationshipsRequest,
     CheckPermissionRequest,
     CheckPermissionResponse,
-    Client,
+    AsyncClient,
     Consistency,
     ObjectReference,
     Relationship,
@@ -49,96 +49,100 @@ definition note {
 """
 
 def get_client() -> Client:
-    return Client(
+    return AsyncClient(
         "localhost:50051",
         insecure_bearer_token_credentials("somerandomkeyhere")
     )
 
-# real system would use actual IDs as object_id
-emilia = SubjectReference(
-    object=ObjectReference(
-        object_type="user",
-        object_id="emilia"
+
+async def main():
+    # real system would use actual IDs as object_id
+    emilia = SubjectReference(
+        object=ObjectReference(
+            object_type="user",
+            object_id="emilia"
+        )
     )
-)
 
-alfred = SubjectReference(
-    object=ObjectReference(
-        object_type="user",
-        object_id="alfred"
+    alfred = SubjectReference(
+        object=ObjectReference(
+            object_type="user",
+            object_id="alfred"
+        )
     )
-)
 
-alfreds_daily_note = ObjectReference(
-    object_type="note",
-    object_id="daily_note_2026-03-10"
-)
-
-client = get_client()
-client.WriteSchema(WriteSchemaRequest(schema=SCHEMA_ZED))
-
-requests = [
-    BulkImportRelationshipsRequest(
-        relationships=[
-            Relationship(
-                resource=alfreds_daily_note,
-                relation="admin",
-                subject=alfred,
-            ),
-            Relationship(
-                resource=alfreds_daily_note,
-                relation="reader",
-                subject=emilia
-            )
-        ]
+    alfreds_daily_note = ObjectReference(
+        object_type="note",
+        object_id="daily_note_2026-03-10"
     )
-]
 
-import_requests = client.BulkImportRelationships((req for req in requests))
-assert import_requests.num_loaded == 2
+    client = get_client()
+    await client.WriteSchema(WriteSchemaRequest(schema=SCHEMA_ZED))
 
-export_requests = client.BulkExportRelationships(
-    BulkExportRelationshipsRequest(
-        consistency=Consistency(fully_consistent=True)
+    requests = [
+        BulkImportRelationshipsRequest(
+            relationships=[
+                Relationship(
+                    resource=alfreds_daily_note,
+                    relation="admin",
+                    subject=alfred,
+                ),
+                Relationship(
+                    resource=alfreds_daily_note,
+                    relation="reader",
+                    subject=emilia
+                )
+            ]
+        )
+    ]
+
+    import_requests = await client.BulkImportRelationships((req for req in requests))
+    assert import_requests.num_loaded == 2
+
+    export_requests = client.BulkExportRelationships(
+        BulkExportRelationshipsRequest(
+            consistency=Consistency(fully_consistent=True)
+        )
     )
-)
 
-relationships = []
-for resp in export_requests:
-    for rel in resp.relationships:
-        relationships.append(rel)
+    relationships = []
+    async for resp in export_requests:
+        for rel in resp.relationships:
+            relationships.append(rel)
 
-assert len(relationships) == 2
+    assert len(relationships) == 2
 
-# make permission check
+    # make permission check
 
-# direct permission
-resp = client.CheckPermission(CheckPermissionRequest(
-    resource=alfreds_daily_note,
-    permission="delete",
-    subject=alfred
-))
-assert resp.permissionship == CheckPermissionResponse.PERMISSIONSHIP_HAS_PERMISSION
+    # direct permission
+    resp = await client.CheckPermission(CheckPermissionRequest(
+        resource=alfreds_daily_note,
+        permission="delete",
+        subject=alfred
+    ))
+    assert resp.permissionship == CheckPermissionResponse.PERMISSIONSHIP_HAS_PERMISSION
 
-# computed permission
-resp = client.CheckPermission(CheckPermissionRequest(
-    resource=alfreds_daily_note,
-    permission="view",
-    subject=alfred
-))
-assert resp.permissionship == CheckPermissionResponse.PERMISSIONSHIP_HAS_PERMISSION
-resp = client.CheckPermission(CheckPermissionRequest(
-    resource=alfreds_daily_note,
-    permission="view",
-    subject=emilia
-))
-assert resp.permissionship == CheckPermissionResponse.PERMISSIONSHIP_HAS_PERMISSION
+    # computed permission
+    resp = await client.CheckPermission(CheckPermissionRequest(
+        resource=alfreds_daily_note,
+        permission="view",
+        subject=alfred
+    ))
+    assert resp.permissionship == CheckPermissionResponse.PERMISSIONSHIP_HAS_PERMISSION
+    resp = await client.CheckPermission(CheckPermissionRequest(
+        resource=alfreds_daily_note,
+        permission="view",
+        subject=emilia
+    ))
+    assert resp.permissionship == CheckPermissionResponse.PERMISSIONSHIP_HAS_PERMISSION
 
-# emilia should not be able to delete alfreds note
-resp = client.CheckPermission(CheckPermissionRequest(
-    resource=alfreds_daily_note,
-    permission="delete",
-    subject=emilia
-))
-assert resp.permissionship == CheckPermissionResponse.PERMISSIONSHIP_NO_PERMISSION
-print("Tests finished")
+    # emilia should not be able to delete alfreds note
+    resp = await client.CheckPermission(CheckPermissionRequest(
+        resource=alfreds_daily_note,
+        permission="delete",
+        subject=emilia
+    ))
+    assert resp.permissionship == CheckPermissionResponse.PERMISSIONSHIP_NO_PERMISSION
+    print("Tests finished")
+
+asyncio.run(main())
