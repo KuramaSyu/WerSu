6999fd6006735498b16f2ca6d1061cca883457fd
kurama <paul.zenker.pz@gmail.com>
2026-03-13 12:52:11 +0100

    refactor,feat: added and implemented NotePermissionRepo
    #14

--- src/api/user_context.py
+++ src/api/user_context.py
@@ -3,38 +3,8 @@ from typing import Any
 
 class UserContextABC(ABC):
     @abstractmethod
-    def lookp_subjects(action: str, subjecttype: str, subjectid: str) -> list[Any]:
-        """Lookup subjects for a given action, subject type, and subject ID.
-        
-        Example
-        --------
-        Lookup for user:alice#view -> `lookp_subjects("view", "doc", "notebook1")` -> ["user:alice", "user:bob"]
-        """
-        pass
-
-    @abstractmethod
     def get_user_id(self) -> str:
-        """Get the user ID of the current user."""
-        pass
-
-    @abstractmethod
-    def lookup_notes(self, action: str) -> list[Any]:
-        """Lookup notes for a given action for the current user using `get_user_id()`.
-        
-        Example
-        --------
-        Lookup for view -> `lookup_notes("view")` -> ["note1", "note2"]
-        """
-        pass
-
-    @abstractmethod
-    def add_subject(self, subject: str, subjectid: str) -> None:
-        """Add a subject to the user context.
-        
-        Example
-        --------
-        Add doc:notebook1 -> `add_subject("doc", "notebook1")` adds the subject notebook1 for the current user using `get_user_id()`.
-        """
-        pass
+        """Returns the user ID of the current user"""
+        ...
 
 
--- src/db/repos/note/permission.py
+++ src/db/repos/note/permission.py
@@ -1,133 +1,260 @@
 from abc import ABC, abstractmethod
-from typing import List
+from copy import deepcopy
+from operator import ge
+import re
+from typing import Any, List, Protocol
 
 from asyncpg import Record
+from authzed.api.v1.permission_service_pb2 import ImportBulkRelationshipsRequest
+from src.api.user_context import UserContextABC
 from src.db.entities import NotePermissionEntity
+from src.db.repos.note.note import UserContext
 from src.db.table import TableABC
-
 from src.utils import asdict
 
+from authzed.api.v1 import (
+    BulkExportRelationshipsRequest,
+    BulkImportRelationshipsRequest,
+    CheckPermissionRequest,
+    CheckPermissionResponse,
+    AsyncClient,
+    Consistency,
+    DeleteRelationshipsRequest,
+    LookupResourcesRequest,
+    ObjectReference,
+    Relationship as SpicedbRelationship,
+    RelationshipFilter,
+    SubjectFilter,
+    SubjectReference,
+    WriteSchemaRequest,
+)
+from grpcutil import insecure_bearer_token_credentials
+
+from src.api import UNDEFINED, UndefinedNoneOr, UndefinedOr
+
+class ObjectRef:
+    def __init__(self, object_type: str, object_id: UndefinedOr[str]) -> None:
+        self.object_type = object_type
+        self.object_id = object_id
+      
+
+class SubjectRef(ObjectRef):
+    pass
+
+class Relationship:
+    def __init__(self, object: ObjectRef, relation: str, subject: SubjectRef) -> None:
+        self.object = object
+        self.relation = relation
+        self.subject = subject
+
+class PermissionConverterABC(ABC):
+
+    @abstractmethod
+    def convert_object_ref(self, object_ref: ObjectRef) -> Any:
+        ...
+
+    @abstractmethod
+    def convert_subject_ref(self, subject_ref: SubjectRef) -> Any:
+        ...
+
+    @abstractmethod
+    def convert_relationship(self, relationship: Relationship) -> Any:
+        ...
+
+class SpicedbPermissionConverter(PermissionConverterABC):
+    def convert_object_ref(self, object_ref: ObjectRef) -> ObjectReference:
+        assert object_ref.object_id == str, "object_id must be provided for object reference"
+        return ObjectReference(
+            object_type=object_ref.object_type,
+            object_id=str(object_ref.object_id)
+        )
+
+    def convert_subject_ref(self, subject_ref: SubjectRef) -> SubjectReference:
+        return SubjectReference(
+            object=self.convert_object_ref(subject_ref)
+        )
+    
+    def convert_relationship(self, relationship: Relationship) -> SpicedbRelationship:
+        return SpicedbRelationship(
+            resource=self.convert_object_ref(relationship.object),
+            relation=relationship.relation,
+            subject=self.convert_subject_ref(relationship.subject)
+        )
 
 class NotePermissionRepo(ABC):
 
     @abstractmethod
     async def insert(
         self,
-        permission: NotePermissionEntity,
-    ) -> NotePermissionEntity:
+        relationships: List[Relationship]
+    ) -> List[Relationship]:
         """inserts permission
         
         Args:
         -----
-        permission: `NotePermissionEntity`
-            the permission of a note
+        relationships: `List[Relationship]`
+            the relationships to insert. Provide full subject, permission and object.
 
         Returns:
         --------
-        `NotePermissionEntity`:
-            the updated entity (updated ID)
-        """
-        ...
-
-    @abstractmethod
-    async def update(
-        self,
-        set: NotePermissionEntity,
-        where: NotePermissionEntity,
-    ) -> NotePermissionEntity:
-        """updates permission
-        
-        Args:
-        -----
-        set: `NotePermissionEntity`
-            the fields to update
-        where: `NotePermissionEntity`
-            the conditions to match
-
-        Returns:
-        --------
-        `NotePermissionEntity`:
-            the updated entity
+        `List[Relationship]`:
+            the inserted relationships
         """
         ...
 
     @abstractmethod
     async def delete(
         self,
-        permission: NotePermissionEntity,
-    ) -> NotePermissionEntity:
+        relationship: Relationship
+    ) -> Relationship:
         """delete permission
         
         Args:
         -----
-        permission: `NotePermissionEntity`
-            the permission of a note
+        relationships: `List[Relationship]`
+            the relationships to delete. Provide full subject and permission, and for object leave the 
+            object_id to `UNDEFINED` and only provide the object_type. This allows to delete all permissions 
+            for a given subject and permission on all objects of a given type.
 
         Returns:
         --------
-        `NotePermissionEntity`:
-            the deleted entity
+        `List[Relationship]`:
+            the deleted relationships
         """
         ...
 
     @abstractmethod
-    async def select(
+    async def lookup(
         self,
-        permission: NotePermissionEntity,
-    ) -> List[NotePermissionEntity]:
+        relationship: Relationship
+    ) -> List[ObjectRef]:
         """select permission
         
         Args:
         -----
-        permission: `NotePermissionEntity`
-            the permission of a note to search for
+        relationship: `Relationship`
+            the relationship to lookup. Provide full subject and permission, and for object leave the 
+            object_id to `UNDEFINED` and only provide the object_type. This allows to lookup all permissions 
+            for a given subject and permission on all objects of a given type.
 
         Returns:
         --------
-        `List[NotePermissionEntity]`:
-            the matching entities
+        `List[ObjectRef]`:
+            the matching objects
         """
         ...
 
+    @abstractmethod
+    async def lookup_notes(
+        self,
+        user: UserContextABC,
+        permission: str
+    ) -> List[ObjectRef]:
+        """Retrieves all notes where the given user has the given permission
+        
+        Args:
+        -----
+        user: `UserContextABC`
+            the user context to lookup permissions for
+        permission: `str`
+            the permission to lookup
 
-class NotePermissionPostgresRepo(NotePermissionRepo):
-    """Provides an implementation using Postgres as the backend database"""
-    def __init__(self, table: TableABC[List[Record]]):
-        self._table = table
+        Returns:
+        --------
+        `List[ObjectRef]`:
+            the matching objects
+        """
+        ...
 
-    async def insert(self, permission: NotePermissionEntity) -> NotePermissionEntity:
-        record = await self._table.insert(asdict(permission))
-        if not record:
-            raise Exception("Failed to insert permission")
-        return permission
-
-    async def update(self, set: NotePermissionEntity, where: NotePermissionEntity) -> NotePermissionEntity:
-        record = await self._table.update(
-            set=asdict(set),
-            where=asdict(where)
-        )
-        if not record:
-            raise Exception("Failed to update permission")
-        return set
-
-    async def delete(self, permission: NotePermissionEntity) -> NotePermissionEntity:
-        conditions = asdict(permission)
-        if not conditions:
-            raise ValueError(f"At least one field must be set to delete a permission: {permission}")
-        record = await self._table.delete(
-            where=conditions
-        )
-        if not record:
-            raise Exception("Failed to delete permission")
-        return permission
-    
-    async def select(self, permission: NotePermissionEntity) -> List[NotePermissionEntity]:
-        records = await self._table.select(
-            where=asdict(permission)
-        )
-        if not records:
-            return []
-        return [NotePermissionEntity(**record) for record in records]
     
 class NotePermissionSpicedbRepo(NotePermissionRepo):
-    ...
+    converter = SpicedbPermissionConverter()
+
+    def __init__(self, client: AsyncClient) -> None:
+        self.client = client
+
+    async def insert(self, relationships: List[Relationship]) -> List[Relationship]:
+        requests = [ImportBulkRelationshipsRequest(
+            relationships=[self.converter.convert_relationship(rel) for rel in relationships]
+        )]
+        await self.client.ImportBulkRelationships((req for req in requests))
+        return relationships
+
+    async def delete(self, relationship: Relationship) -> Relationship:
+        # spicedb does not support bulk delete, so we need to delete one by one
+        deleted_relationships = []
+        filter = RelationshipFilter()
+        
+        def get_filter(rel: Relationship) -> RelationshipFilter:
+            assert rel.object.object_id != UNDEFINED, "object_id must be provided for delete operation"
+            assert rel.subject.object_id != UNDEFINED, "subject_id must be provided for delete operation"
+            
+            filter = RelationshipFilter(
+                resource_type=rel.object.object_type,
+                optional_resource_id=str(rel.object.object_id),
+                optional_relation=rel.relation,
+                optional_subject_filter=SubjectFilter(
+                    subject_type=rel.subject.object_type,
+                    optional_subject_id=str(rel.subject.object_id)
+                )
+            )
+            return filter
+       
+        
+        result = await self.client.DeleteRelationships(
+            DeleteRelationshipsRequest(
+                relationship_filter=get_filter(relationship)
+            )
+        )
+        
+        assert result.DELETION_PROGRESS_COMPLETE
+        return relationship
+
+    async def lookup(self, relationship: Relationship) -> List[ObjectRef]:
+        # spicedb does not support bulk lookup, so we need to lookup one by one
+        filter = RelationshipFilter(
+            optional_subject_filter=SubjectFilter(
+                subject_type=relationship.subject.object_type,
+                optional_subject_id=str(relationship.subject.object_id)
+            )
+        )
+        filter.optional_relation = relationship.relation
+        filter.resource_type = relationship.object.object_type
+        if relationship.object.object_id != UNDEFINED:
+            filter.optional_resource_id = str(relationship.object.object_id)
+        if relationship.subject.object_id != UNDEFINED:
+            filter.optional_subject_filter
+        
+        result = self.client.LookupResources(
+            LookupResourcesRequest(
+                resource_object_type=relationship.object.object_type,
+                permission=relationship.relation,
+                subject=self.converter.convert_subject_ref(relationship.subject),
+                consistency=Consistency(fully_consistent=True)
+            )   
+        )
+        objects: List[ObjectRef] = []
+        async for resp in result:
+            objects.append(
+                ObjectRef(
+                    object_type=relationship.object.object_type,
+                    object_id=resp.resource_object_id
+                )
+            )
+        return objects
+
+    async def lookup_notes(self, user: UserContextABC, permission: str) -> List[ObjectRef]:
+        user_id = user.get_user_id()
+        relationship = Relationship(
+            object=ObjectRef(
+                object_type="note",
+                object_id=UNDEFINED
+            ),
+            relation=permission,
+            subject=SubjectRef(
+                object_type="user",
+                object_id=user_id
+            )
+        )
+        return await self.lookup(relationship)
+
