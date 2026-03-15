f60cab60aa6b7e9c9ddfb67f40aa6461dc1cfe30
kurama <paul.zenker.pz@gmail.com>
2026-03-15 13:13:47 +0100

    api: use property instead of getter

--- src/api/user_context.py
+++ src/api/user_context.py
@@ -2,8 +2,10 @@ from abc import ABC, abstractmethod
 from typing import Any
 
 class UserContextABC(ABC):
+
+    @property
     @abstractmethod
-    def get_user_id(self) -> str:
+    def user_id(self) -> str:
         """Returns the user ID of the current user"""
         ...
 
