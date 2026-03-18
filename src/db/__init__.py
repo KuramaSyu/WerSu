bf3d8ecd69a22b7702030dd149585c23fd76cbee
kurama <paul.zenker.pz@gmail.com>
2026-03-18 12:58:31 +0100

    Revert "refactor: better explicit import loading" imports
    This reverts commit 1aea0e593ae3d9b263aa5d559002f89fb9f3af6e.

--- src/db/__init__.py
+++ src/db/__init__.py
@@ -1,11 +1,4 @@
 from .database import Database
 from .table import Table, TableABC
-
-
-def __getattr__(name: str):
-	if name in {"repos", "entities"}:
-		from importlib import import_module
-
-		return import_module(f"{__name__}.{name}")
-
-	raise AttributeError(f"module {__name__!r} has no attribute {name!r}")
+from .repos import *
+from .entities import *
--- src/db/repos/__init__.py
+++ src/db/repos/__init__.py
@@ -1,7 +1,2 @@
-def __getattr__(name: str):
-	if name in {"note", "user"}:
-		from importlib import import_module
-
-		return import_module(f"{__name__}.{name}")
-
-	raise AttributeError(f"module {__name__!r} has no attribute {name!r}")
+from .note import *
+from .user import *
