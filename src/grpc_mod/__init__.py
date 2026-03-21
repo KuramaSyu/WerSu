afe03606bf6725614f952fd1f9de6632faeb2c59
kurama <paul.zenker.pz@gmail.com>
2026-03-21 14:38:14 +0100

    Update __init__.py

--- src/grpc_mod/__init__.py
+++ src/grpc_mod/__init__.py
@@ -1,8 +1,29 @@
-from .proto.note_pb2_grpc import add_NoteServiceServicer_to_server, NoteService, NoteServiceServicer, NoteServiceStub
+from .proto.note_pb2_grpc import (
+    add_NoteServiceServicer_to_server,
+    add_PermissionServiceServicer_to_server,
+    NoteService,
+    NoteServiceServicer,
+    NoteServiceStub,
+    PermissionService,
+    PermissionServiceServicer,
+    PermissionServiceStub,
+)
 from .proto.note_pb2 import (
-    GetNoteRequest, Note, NotePermission, 
-    PostNoteRequest, NoteEmbedding, GetSearchNotesRequest,
-    MinimalNote
+    CreatePermissionRequest,
+    DeletePermissionRequest,
+    GetNoteRequest,
+    GetPermissionsRequest,
+    GetSearchNotesRequest,
+    MinimalNote,
+    Note,
+    NoteEmbedding,
+    NotePermission,
+    PermissionObjectType,
+    PermissionRelationship,
+    PermissionSubject,
+    PermissionsResponse,
+    PostNoteRequest,
+    ReplacePermissionsRequest,
 )
 from .proto.user_pb2_grpc import add_UserServiceServicer_to_server, UserService, UserServiceServicer, UserServiceStub
 from .proto.user_pb2 import User, GetUserRequest, AlterUserRequest, DeleteUserRequest, DeleteUserResponse, PostUserRequest
