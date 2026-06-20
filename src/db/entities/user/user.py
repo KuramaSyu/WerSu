f9bd51c27772d287ec889ca109fa202897e7dbce
kurama <52566613+KuramaSyu@users.noreply.github.com>
2026-06-20 20:36:51 +0200

    api!: discord id is now optional and user_kind is new

--- src/db/entities/user/user.py
+++ src/db/entities/user/user.py
@@ -1,12 +1,15 @@
 from dataclasses import dataclass
-from typing import Optional
+from typing import Literal, Optional
+
+from src.api.undefined import UndefinedOr
 
 
 @dataclass
 class UserEntity:
-    discord_id: int
+    discord_id: Optional[int] = None
     avatar: Optional[str] = None
     id: Optional[str] = None
     username: Optional[str] = None
     discriminator: Optional[str] = None
     email: Optional[str] = None
+    user_kind: UndefinedOr[Literal["human", "temporary", "system"]] = None
