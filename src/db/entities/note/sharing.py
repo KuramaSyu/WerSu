75a271df4af115637296e7c7918b6a60170c19cd
kurama <52566613+KuramaSyu@users.noreply.github.com>
2026-06-20 20:55:28 +0200

    api!: use datetime if possible

--- src/db/entities/note/sharing.py
+++ src/db/entities/note/sharing.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+from datetime import datetime
 
 
 from src.api.undefined import UNDEFINED, UndefinedOr, UndefinedNoneOr
@@ -6,7 +7,9 @@ from src.api.undefined import UNDEFINED, UndefinedOr, UndefinedNoneOr
 
 @dataclass
 class NoteShareEntity:
-    """Represents a share for a note"""
+    """Represents a share for a note. 
+    Use UNDEFINED in case a field is not yet set. Use None, to explicitly set the field to None/Null
+    """
     # the id of the share (uuidv7)
     id: UndefinedOr[str] = UNDEFINED
 
@@ -17,16 +20,16 @@ class NoteShareEntity:
     note_id: UndefinedOr[str] = UNDEFINED
 
     # when the share was created
-    created_at: str
+    created_at: UndefinedOr[datetime] = UNDEFINED
 
     # who created the share (user id)
-    created_by: str
+    created_by: UndefinedOr[datetime] = UNDEFINED
 
     # when the share is online e.g. accessible from outside
-    online_since: str | None
+    online_since: UndefinedNoneOr[datetime] = UNDEFINED
 
     # until when the share is online e.g. accessible from outside. If None, the share does not expire.
-    online_until: str | None
+    online_until: UndefinedNoneOr[datetime] = UNDEFINED
 
     # the user under which to access the note linked with note_id. The logged in user,
     # if not granted permission directly, can not access this note. The logged in user
