800273918ed876e42826c2ad2567be612bd3351c
kurama <52566613+KuramaSyu@users.noreply.github.com>
2026-07-27 21:12:48 +0200

    Update __init__.py

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -3,3 +3,4 @@ from .convert import asdict, convert_entity_for_db
 from .dict_helper import drop_undefined, drop_except_keys
 from .logging import logging_provider
 from .attachment_url import build_attachment_url
+from .record_helpers import all_valid_items
